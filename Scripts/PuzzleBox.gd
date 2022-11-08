10d69822b1d17b30e4549a00ba5c80a0edc9edc3
Nick Cunningham <thenickoftime.x@gmail.com>
2022-11-07 22:29:33 -0800

    Working on camera stuff

--- Scripts/PuzzleBox.gd
+++ Scripts/PuzzleBox.gd
@@ -1,18 +1,29 @@
 extends MeshInstance3D
 
-signal finished_rotating
+#Export variables---------------------------------------------------------------
+@export var camera : Camera3D
+@export var marker : Node3D
 
-var rotate_time : float = 0.75
+#OnReady variables--------------------------------------------------------------
 @onready var rotate_degrees : float = deg_to_rad(90)
 
-# Called when the node enters the scene tree for the first time.
+#Member Variables---------------------------------------------------------------
+var rotate_time : float = 0.75
+var is_rotating : bool = false
+
+var mouse_delta : Vector2
+var pan_speed : float = 0.25
+var pan_direction : int = 1
+var is_panning : bool = false
+
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
 	pass
+
+
+func _process(delta):
+	if(is_panning):
+		pan_cube(delta)
 
 
 func _input(event):
@@ -21,18 +32,47 @@ func _input(event):
 		var new_rot : Transform3D
 		
 		if key.keycode == KEY_UP:
-			new_rot = transform.rotated(Vector3.RIGHT, rotate_degrees)
+			new_rot = transform.rotated_local(Vector3.RIGHT, rotate_degrees)
 		elif key.keycode == KEY_DOWN:
-			new_rot = transform.rotated(Vector3.RIGHT, -rotate_degrees)
+			new_rot = transform.rotated_local(Vector3.RIGHT, -rotate_degrees)
 		elif key.keycode == KEY_LEFT:
-			new_rot = transform.rotated(Vector3.UP, rotate_degrees)
+			new_rot = transform.rotated_local(Vector3.FORWARD, rotate_degrees)
 		elif key.keycode == KEY_RIGHT:
-			new_rot = transform.rotated(Vector3.UP, -rotate_degrees)
+			new_rot = transform.rotated_local(Vector3.FORWARD, -rotate_degrees)
 		
+		rotate_direction()
 		rotate_cube(new_rot)
+	
+	if event is InputEventMouseButton:
+		var mouse_button = event as InputEventMouseButton
+		is_panning = mouse_button.is_pressed()
+	
+	elif event is InputEventMouseMotion:
+		var mouse_motion = event as InputEventMouseMotion
+		mouse_delta = event.relative
 
 func rotate_cube(new_rotation : Transform3D):
+	if is_rotating: return
+	is_rotating = true
+	
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "transform", new_rotation, rotate_time)
 	
 	await tween.finished
+	is_rotating = false
+
+
+func rotate_direction():
+	var new_rot : Transform3D
+	var cube_dir : Vector3 = transform.basis.z
+	var camera_dir : Vector3 = Vector3(camera.basis.z.x, 0, camera.basis.z.z)
+#	var new_dir : Vector3 = (cube_dir - camera_dir).normalized()
+	
+#	marker.position = new_dir
+	
+	
+	print(cube_dir.angle_to(camera_dir))
+
+
+func pan_cube(delta : float):
+	rotate_y(mouse_delta.x * pan_direction * pan_speed * delta)
