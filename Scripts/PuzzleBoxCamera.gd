5214ce704604ab316747b8f8fd07e0cf6757820c
Nick Cunningham <10342737+TheNickOfTime@users.noreply.github.com>
2022-11-10 22:50:19 -0800

    Worked on camera controls, also rotating the cube based on the direction of the camera

--- Scripts/PuzzleBoxCamera.gd
+++ Scripts/PuzzleBoxCamera.gd
@@ -5,15 +5,18 @@ var mouse_delta : Vector2
 var pan_speed : float = 0.25
 var pan_direction : int = -1
 var is_panning : bool = false
+var max_angle : float = 45
 
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 func _process(delta):
 	if(is_panning):
 		pan_cube(delta)
+	
+	mouse_delta = Vector2.ZERO
 
 
 func _input(event):
@@ -21,10 +24,12 @@ func _input(event):
 		var mouse_button = event as InputEventMouseButton
 		is_panning = mouse_button.is_pressed()
 	
-	elif event is InputEventMouseMotion:
-		# var mouse_motion = event as InputEventMouseMotion
+	if event is InputEventMouseMotion:
 		mouse_delta = event.relative
+		
 
 
 func pan_cube(delta : float):
 	rotate_y(mouse_delta.x * pan_direction * pan_speed * delta)
+	rotate_object_local(Vector3.RIGHT, mouse_delta.y * pan_direction * pan_speed * delta)
+	rotation.x = clamp(rotation.x, deg_to_rad(-max_angle), deg_to_rad(0))
